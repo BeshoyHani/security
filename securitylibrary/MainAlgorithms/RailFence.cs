04a518665ce9396bf1ef1f2931a511a56aecb612
Riva1412 <53063436+Riva1412@users.noreply.github.com>
2022-03-12 01:46:59 +0200

    Rail Fence Cipher

--- securitylibrary/MainAlgorithms/RailFence.cs
+++ securitylibrary/MainAlgorithms/RailFence.cs
@@ -10,17 +10,58 @@ namespace SecurityLibrary
     {
         public int Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            int n = cipherText.Length;
+            for (int key = 1; key <= n; key++)
+            {
+                string tmp = Encrypt(plainText, key);
+                if (tmp.Equals(cipherText, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return key;
+                }
+            }
+            return 0;
         }
 
         public string Decrypt(string cipherText, int key)
         {
-            throw new NotImplementedException();
+            int n = cipherText.Length;
+            int mov = 0;
+            string ans = "";
+            for (int i = 0; i < n; i++)
+            {
+                ans += ' ';
+            }
+            int cnt = 1,x=0;
+            StringBuilder sb = new StringBuilder(ans);
+            for (int i = 0; i < n; i++)
+            {
+                sb[x] = cipherText[i];
+                x += key;
+                if (x >= n)
+                {
+                    x = cnt++;
+                }
+            }
+            ans = sb.ToString();
+
+            return ans;
+
         }
 
         public string Encrypt(string plainText, int key)
         {
-            throw new NotImplementedException();
+            int x = plainText.Length%key;
+          
+            int n = plainText.Length;
+            string ans = "";
+            for (int i = 0; i < key; i++)
+            {
+                for(int j = i; j < n ; j += key)
+                {
+                    ans += plainText[j];
+                }
+            }
+            return ans;
         }
     }
 }
